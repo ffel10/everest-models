9da8f0ad20bcb89208528f72aaf173bae6a9bce3
ffel10 <felix_feldmann@gmx.de>
2026-08-04 10:17:38 +0200

    Adjust job exports in __init__

--- src/everest_models/jobs/__init__.py
+++ src/everest_models/jobs/__init__.py
@@ -16,7 +16,7 @@ from everest_models.jobs import (
     fm_well_constraints,
     fm_well_filter,
     fm_well_swapping,
-    fm_well_trajectory, 
+    fm_well_trajectory,
 )
 
 __all__ = [
@@ -37,5 +37,5 @@ __all__ = [
     "fm_well_constraints",
     "fm_well_filter",
     "fm_well_trajectory",
-    "fm_well_swapping", 
+    "fm_well_swapping",
 ]
